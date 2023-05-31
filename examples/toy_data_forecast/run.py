6f4e6faa918a73bbd8b93d963d42368c40034c6c
Patrick Bray <patrickbray17@gmail.com>
2023-05-31 12:15:28 -0400

    get eval working

--- examples/toy_data_forecast/run.py
+++ examples/toy_data_forecast/run.py
@@ -2,10 +2,11 @@ import hydra
 import torch
 import torch.nn as nn
 from hydra.utils import instantiate
-
+from pathlib import Path
+import os
 from tsa import AutoEncForecast, train, evaluate
 from tsa.utils import load_checkpoint
-
+import mlflow
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -17,13 +18,16 @@ def run(cfg):
     model = AutoEncForecast(cfg.training, input_size=nb_features).to(device)
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.training.lr)
-
-    if cfg.general.do_train:
-        train(train_iter, test_iter, model, criterion, optimizer, cfg, ts)
-    if cfg.general.do_eval and cfg.general.get("ckpt", False):
-        model, _, loss, epoch = load_checkpoint(cfg.general.ckpt, model, optimizer, device)
-        evaluate(test_iter, loss, model, cfg, ts)
-    print('complete')
+    mlflow.set_tracking_uri(uri="../../../mlruns") # TODO: figure out the cwd situation
+    mlflow.set_experiment(experiment_name="prediction")
+    mlflow.pytorch.autolog()
+    with mlflow.start_run() as run:
+        if cfg.general.do_train:
+            train(train_iter, test_iter, model, criterion, optimizer, cfg, ts)
+        if cfg.general.do_eval and cfg.general.get("ckpt", False):
+            model, _, loss, epoch = load_checkpoint(Path(os.getcwd()) / cfg.general.output_dir / cfg.general.ckpt, model, optimizer, device)
+            evaluate(test_iter, loss, model, cfg, ts)
+        print('complete')
 
 
 if __name__ == "__main__":
